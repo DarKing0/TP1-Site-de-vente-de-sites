82d258db53fb84a649d27c80078bada85a6db927
DarKing01 <zasirissouf.jr@gmail.com>
2024-10-10 17:59:45 +0200

    days3

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
  <?php
 /* base mariadb : port 3306
 $configDatabase = [
-    'dbname'  => 'immobat',
-    'user'    => 'AppPika',
-    'pwd'     => '@BTS@sio2020',
+    'dbname'  => '',
+    'user'    => '',
+    'pwd'     => '',
     'host'    => 'localhost',
     'port'    => '49666',
     'charset' => 'UTF8',
